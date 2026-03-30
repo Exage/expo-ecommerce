b8c66984ecc8b0f5175f631be758cac9410318e5
Nikita Gorkavchuk <horkavchuk.nik@gmail.com>
2026-03-30 22:49:45 +0300

    add root route

--- backend/src/server.js
+++ backend/src/server.js
@@ -54,6 +54,10 @@ app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
 });
 
+app.get("/api/", (req, res) => {
+  res.status(200).send("Backend server is running");
+});
+
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
